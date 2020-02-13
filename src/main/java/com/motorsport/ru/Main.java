fbb51dbead3254ba16d103a1f8b335105801f0a0
Roman Nemchinov <nemchinovrp@yandex.ru>
2020-02-13 17:18:26 +0300

    Read file

--- src/main/java/com/motorsport/ru/Main.java
+++ src/main/java/com/motorsport/ru/Main.java
@@ -1,10 +1,17 @@
 package com.motorsport.ru;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ReadFileService readFileService = new ReadFileService();
+        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
+        readFileService.readFileAndPutInQueue("file.txt", queue);
+
+
         ExecutorService e = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         e.execute(new Runnable() {
             public void run() {
--- src/main/java/com/motorsport/ru/ReadFileService.java
+++ src/main/java/com/motorsport/ru/ReadFileService.java
@@ -5,41 +5,29 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class ReadFileService {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
+        new ReadFileService().readFileAndPutInQueue("file.txt", queue);
+        System.out.println(queue.size());
+        queue.stream().forEach(System.out::println);
 
-        ReadFileService main = new ReadFileService();
-        File file = main.getFileFromResources("file.txt");
-
-        printFile(file);
     }
 
-    private File getFileFromResources(String fileName) {
-
+    public LinkedBlockingQueue readFileAndPutInQueue(String fileName, LinkedBlockingQueue<String> queue) throws IOException, InterruptedException {
         ClassLoader classLoader = getClass().getClassLoader();
-
         URL resource = classLoader.getResource(fileName);
-        if (resource == null) {
-            throw new IllegalArgumentException("file is not found!");
-        } else {
-            return new File(resource.getFile());
-        }
-
-    }
-
-    private static void printFile(File file) throws IOException {
-
-        if (file == null) return;
-
+        assert resource != null;
+        File file = new File(resource.getFile());
         try (FileReader reader = new FileReader(file);
              BufferedReader br = new BufferedReader(reader)) {
-
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                queue.put(line);
             }
         }
+        return queue;
     }
-
 }
